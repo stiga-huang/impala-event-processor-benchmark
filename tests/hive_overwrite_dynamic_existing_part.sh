1e0ca940016dd74da4d8b20b76c23b677daac648
stiga-huang <huangquanlong@gmail.com>
2025-05-27 23:38:42 +0800

    Fix insert overwrite syntax error on old hive versions

--- tests/hive_overwrite_dynamic_existing_part.sh
+++ tests/hive_overwrite_dynamic_existing_part.sh
@@ -5,7 +5,7 @@ procuder() {
   echo "$(get_ts) Impala> Adding new rows to partition p=200..399 so each has more than one row"
   $IMPALA_EXEC "insert into $DB.$TBL partition(p) select $COLS,cast(p+200 as int) from $DB.tbl3 where p<200"
   echo "$(get_ts) Hive> Dynamically overwriting 200 partitions"
-  $HIVE_EXEC "set hive.stats.autogather=false; set hive.exec.max.dynamic.partitions.pernode=200; insert overwrite $DB.$TBL select $COLS, p+200 from $DB.tbl3 where p<200"
+  $HIVE_EXEC "set hive.stats.autogather=false; set hive.exec.max.dynamic.partitions.pernode=200; insert overwrite table $DB.$TBL select $COLS, p+200 from $DB.tbl3 where p<200"
 }
 
 consumer_verified() {
